feb98473c53b89bd54abf5ca87bcf532fc32164d
chocobo333 <chocobo1355@gmail.com>
2021-11-23 22:27:45 +0900

    fine change

--- src/sema.nim
+++ src/sema.nim
@@ -72,6 +72,4 @@ proc sema*(node: AstNode, module: Module): ref Term =
     mainid.typ = mainty
     mainid.typ.symbol = some sym
     # mainid.typ.symbol.get.instances[mainid.typ] = Symbol()
-    echo program
-    echo mainScope
     main
