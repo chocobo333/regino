f6bf67a263db7c9bf227f4fdcb83ecf5a1e60ba9
Unit112358 <yunitto55@gmail.com>
2022-04-19 00:04:42 +0900

    Add new errors

--- src/v2/errors.nim
+++ src/v2/errors.nim
@@ -11,6 +11,8 @@ type
         Undeciable
         Undefined
         NoSuite
+        Unasignable
+        Letasign
         SomethingWrong
     TypeError* = object of CatchableError
         loc*: Location
@@ -21,10 +23,13 @@ type
             t1, t2: Value
         of TypeErrorKind.Undeciable:
             nil
-        of TypeErrorKind.Undefined:
+        of TypeErrorKind.Undefined, TypeErrorKind.Letasign:
             id: Ident
         of TypeErrorKind.NoSuite:
             nil
+        of TypeErrorKind.Unasignable:
+            pat: Pattern
+            val: Expression
         of TypeErrorKind.SomethingWrong:
             nil
 
@@ -40,6 +45,10 @@ proc `$`*(self: TypeError): string =
         fmt"ident `{self.id}` is not defined"
     of TypeErrorKind.NoSuite:
         fmt"no block"
+    of TypeErrorKind.Unasignable:
+        fmt"`{self.val}` is not asignable to `{self.pat}`"
+    of TypeErrorKind.Letasign:
+        fmt"`{self.id}` cannot be re-asigned to, because `{self.id}` was defined in let section"
     of TypeErrorKind.SomethingWrong:
         fmt"omething is wrong"
 
@@ -56,5 +65,9 @@ proc Undefined*(_: typedesc[TypeError], id: Ident, loc: Location = newLocation()
     TypeError(kind: TypeErrorKind.Undefined, id: id, loc: loc)
 proc NoSuite*(_: typedesc[TypeError], loc: Location = newLocation()): TypeError =
     TypeError(kind: TypeErrorKind.NoSuite, loc: loc)
+proc Unasignable*(_: typedesc[TypeError], pat: Pattern, val: Expression, loc: Location = newLocation()): TypeError =
+    TypeError(kind: TypeErrorKind.Unasignable, pat: pat, val: val, loc: loc)
+proc Letasign*(_: typedesc[TypeError], id: Ident, loc: Location = newLocation()): TypeError =
+    TypeError(kind: TypeErrorKind.Letasign, id: id, loc: loc)
 proc SomethingWrong*(_: typedesc[TypeError], loc: Location = newLocation()): TypeError =
     TypeError(kind: TypeErrorKind.SomethingWrong, loc: loc)
