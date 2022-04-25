1d126cd1e539041c8e60b806281e7c9f999852cc
Unit112358 <yunitto55@gmail.com>
2022-04-26 02:13:11 +0900

    Impl SCC

--- src/v2/sema/resolveRelations.nim
+++ src/v2/sema/resolveRelations.nim
@@ -14,23 +14,11 @@ import ../utils
 
 type Graph = Table[Value, seq[Value]]
 
-proc reversed*(graph: ref Graph): Graph =
-    var ret: Graph
-    for source, targets in graph:
-        for target in targets:
-            if target in ret:
-                ret[target].add source
-            else:
-                ret[target] = @[source]
-    ret
 
-proc SCC*(graph: ref Graph): seq[seq[Value]] =
-    var
-        reversedGraph = graph.reversed
-        visited: HashSet[Value]
-    @[]
+proc resolve*(env: TypeEnv, v: Value) =
+    discard
 
-proc resolveRelations*(env: TypeEnv) =
+proc resolve*(env: TypeEnv) =
     var graph: Graph
     for (target, source) in env.constraints:
         if source in graph:
