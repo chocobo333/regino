7d14f48d821224ac8b9613bb5b0f76f50dd2a2fc
chocobo333 <chocobo1355@gmail.com>
2022-05-03 19:44:50 +0900

    Add selects

--- src/v2/typeenv.nim
+++ src/v2/typeenv.nim
@@ -17,6 +17,7 @@ type
     TypeEnv* = ref object
         scope*: Scope
         tvs*: HashSet[Value]
+        selects*: HashSet[Value]
         constraints*: seq[Constraint]
         tvconstraints*: seq[Constraint]
         interconstraints*: seq[Constraint]
@@ -43,6 +44,9 @@ template `raise`*(self: TypeEnv, msg: string): untyped =
 proc Var*(_: typedesc[Value], env: TypeEnv): Value =
     result = Value(kind: ValueKind.Var, tv: newTypeVar())
     env.tvs.incl result
+proc Select*(_: typedesc[Value], types: seq[Value], env: TypeEnv): Value =
+    result = Value(kind: ValueKind.Select, types: types.toHashSet, id: getTvId())
+    env.selects.incl result
 
 proc newTypeEnv*(scope: Scope): TypeEnv =
     TypeEnv(
@@ -202,226 +206,226 @@ proc inst*(typ: Value, env: TypeEnv, subs: Table[GenericType, Value] = initTable
 
     # typ
 
-proc `<=`*(t1, t2: Value): bool {.error.}
-proc `<=`*(env: TypeEnv, t1, t2: Value): bool
-proc `<=?`*(env: TypeEnv, t1, t2: Value): Option[seq[Constraint]]
-template setTypeEnv*(env: TypeEnv): untyped =
-    proc `<=`(t1, t2: Value): bool =
-        env.`<=`(t1, t2)
-    proc `<=?`(t1, t2: Value): Option[seq[Constraint]] =
-        env.`<=?`(t1, t2)
-    # proc `==`(t1, t2: Value): bool =
-    #     t1 <= t2 and t2 <= t1
-proc applicable(self: TypeEnv, id: Ident, v: (Value, Value)): bool =
-    setTypeEnv(self)
-    let
-        (s, d) = v
-        con = id.typ
-        v = Value.Arrow(@[s], d)
-    debug v
-    con == v or con <= v
+# proc `<=`*(t1, t2: Value): bool {.error.}
+# proc `<=`*(env: TypeEnv, t1, t2: Value): bool
+# proc `<=?`*(env: TypeEnv, t1, t2: Value): Option[seq[Constraint]]
+# template setTypeEnv(env: TypeEnv): untyped =
+#     proc `<=`(t1, t2: Value): bool =
+#         env.`<=`(t1, t2)
+#     proc `<=?`(t1, t2: Value): Option[seq[Constraint]] =
+#         env.`<=?`(t1, t2)
+#     # proc `==`(t1, t2: Value): bool =
+#     #     t1 <= t2 and t2 <= t1
+# proc applicable(self: TypeEnv, id: Ident, v: (Value, Value)): bool =
+#     setTypeEnv(self)
+#     let
+#         (s, d) = v
+#         con = id.typ
+#         v = Value.Arrow(@[s], d)
+#     debug v
+#     con == v or con <= v
 
 
-proc `<=`*(env: TypeEnv, t1, t2: Value): bool =
-    if t1.kind == ValueKind.Link:
-        env.`<=`(t1.to, t2)
-    elif t2.kind == ValueKind.Link:
-        env.`<=`(t1, t2.to)
-    elif t1.kind == ValueKind.Bottom:
-        true
-    elif t2.kind == ValueKind.Unit:
-        true
-    elif t1 == t2:
-        true
-    elif t1.kind == ValueKind.Gen:
-        env.`<=`(t1.gt.ub, t2)
-    elif t2.isUniv:
-        case t1.kind
-        of ValueKind.Unit:
-            true
-        of ValueKind.Pair:
-            `<=`(env, t1.first, t2) and `<=`(env, t1.second, t2)
-        of ValueKind.Ptr:
-            env.`<=`(t1.pointee, t2)
-        of ValueKind.Singleton:
-            env.`<=`(t1.base, t2)
-        else:
-            if t1.isUniv:
-                t1.litval.level <= t2.litval.level
-            else:
-                debug t1.kind
-                env.raise("notimplemented")
-                false
-    elif t1.kind == t2.kind:
-        case t1.kind
-        of ValueKind.Literal:
-            false
-        of ValueKind.Bottom:
-            true
-        of ValueKind.Unit:
-            true
-        of ValueKind.Bool:
-            true
-        of ValueKind.Integer:
-            t1.bits <= t2.bits
-        of ValueKind.Float:
-            true
-        of ValueKind.Char:
-            true
-        of ValueKind.String:
-            true
-        of ValueKind.Pair:
-            `<=`(env, t1.first, t2.first) and `<=`(env, t1.second, t2.second)
-        of ValueKind.Record:
-            var res = true
-            for key in t2.members.keys:
-                if key in t1.members:
-                    if env.`<=`(t1.members[key], t2.members[key]):
-                        discard
-                    else:
-                        res = false
-                        break
-                else:
-                    res = false
-                    break
-            res
-        of ValueKind.Pi:
-            # TODO: genty
-            let
-                params = t1.params.zip(t2.params).allIt(env.`<=`(it[1], it[0]))
-                rety = env.`<=`(t1.rety, t2.rety)
-            params and rety
-        of ValueKind.Singleton:
-            env.`<=`(t1.base, t2.base)
-        of ValueKind.Var:
-            env.`<=`(t1.tv.ub, t2.tv.lb)
-        of ValueKind.Link:
-            env.`<=`(t1.to, t2.to)
-        else:
-            debug t1.kind
-            env.raise("notimplemented")
-            false
-    # elif t1.kind == ValueKind.Sigma and t2.kind == ValueKind.Sigma:
-    #     `<=`(env, t1.first, t2.first) and `<=`(env, t1.second, t2.second)
-    elif t1.kind == ValueKind.Intersection:
-        t1.types.anyIt(env.`<=`(it, t2))
-    elif t2.kind == ValueKind.Intersection:
-        t2.types.allIt(env.`<=`(t1, it))
-    elif t1.kind == ValueKind.Union:
-        t1.types.allIt(env.`<=`(it, t2))
-    elif t2.kind == ValueKind.Union:
-        t2.types.anyIt(env.`<=`(t1, it))
-    elif t2.kind == ValueKind.Distinct:
-        env.`<=`(t1, t2.base)
-    elif t1.kind == ValueKind.Var:
-        env.`<=`(t1.tv.ub, t2)
-    elif t2.kind == ValueKind.Var:
-        env.`<=`(t1, t2.tv.lb)
-    elif t1.kind == ValueKind.Unit:
-        false
-    elif t2.kind == ValueKind.Bottom:
-        false
-    else:
-        # env.scope.typeOrder.path(t1, t2).isSome
-        let converters = toSeq(env.scope.converters.keys).filterIt(it[1] == t2)
-        converters.anyIt(env.`<=`(t1, it[0]))
-proc `<=?`*(env: TypeEnv, t1, t2: Value): Option[seq[Constraint]] =
-    proc `<=`(t1, t2: Value): bool =
-        env.`<=`(t1, t2)
-    if t1 <= t2:
-        some newSeq[Constraint]()
-    elif t1.kind == t2.kind:
-        case t1.kind
-        of ValueKind.Bottom, ValueKind.Unit, ValueKind.Integer, ValueKind.Float, ValueKind.Char, ValueKind.String:
-            some newSeq[Constraint]()
-        of ValueKind.Pair:
-            let
-                first = env.`<=?`(t1.first, t2.first)
-                second = env.`<=?`(t1.second, t2.second)
-            if first.isSome and second.isSome:
-                some first.get & second.get
-            else:
-                none(seq[Constraint])
-        of ValueKind.Record:
-            let
-                ret = toSeq(t2.members.keys).mapIt(`<=?`(env, t1.members.getOrDefault(it, Value.Unit), t2.members[it]))
-            if ret.allIt(it.isSome):
-                some ret.mapIt(it.get).flatten
-            else:
-                none(seq[Constraint])
-        of ValueKind.Pi:
-            let
-                params = t1.params.zip(t2.params).mapIt(env.`<=?`(it[1], it[0]))
-                rety = env.`<=?`(t1.rety, t2.rety)
-            if params.allIt(it.isSome) and rety.isSome:
-                some params.mapIt(it.get).flatten & rety.get
-            else:
-                none(seq[Constraint])
-        of ValueKind.Var:
-            if t1.tv.lb <= t2.tv.ub:
-                some @[(t1, t2)]
-            else:
-                none(seq[Constraint])
-        else:
-            env.raise("not implemented")
-            none(seq[Constraint])
-    elif t2.kind == ValueKind.Var:
-        # if t2.tv.ub != t1 and t2.tv.ub <= t1:
-        #     none(seq[Constraint])
-        # else:
-        #     some @[(t1, t2)]
-        if t1 <= t2.tv.ub:
-            some @[(t1, t2)]
-        else:
-            none(seq[Constraint])
-    elif t1.kind == ValueKind.Var:
-        # if t2 != t1.tv.lb and t2 <= t1.tv.lb:
-        #     none(seq[Constraint])
-        # else:
-        #     some @[(t1, t2)]
-        if t1.tv.lb <= t2:
-            some @[(t1, t2)]
-        else:
-            none(seq[Constraint])
-    else:
-        if env.scope.typeOrder.path(t1, t2).isSome:
-            some newSeq[Constraint]()
-        else:
-            none(seq[Constraint])
+# proc `<=`*(env: TypeEnv, t1, t2: Value): bool =
+#     if t1.kind == ValueKind.Link:
+#         env.`<=`(t1.to, t2)
+#     elif t2.kind == ValueKind.Link:
+#         env.`<=`(t1, t2.to)
+#     elif t1.kind == ValueKind.Bottom:
+#         true
+#     elif t2.kind == ValueKind.Unit:
+#         true
+#     elif t1 == t2:
+#         true
+#     elif t1.kind == ValueKind.Gen:
+#         env.`<=`(t1.gt.ub, t2)
+#     elif t2.isUniv:
+#         case t1.kind
+#         of ValueKind.Unit:
+#             true
+#         of ValueKind.Pair:
+#             `<=`(env, t1.first, t2) and `<=`(env, t1.second, t2)
+#         of ValueKind.Ptr:
+#             env.`<=`(t1.pointee, t2)
+#         of ValueKind.Singleton:
+#             env.`<=`(t1.base, t2)
+#         else:
+#             if t1.isUniv:
+#                 t1.litval.level <= t2.litval.level
+#             else:
+#                 debug t1.kind
+#                 env.raise("notimplemented")
+#                 false
+#     elif t1.kind == t2.kind:
+#         case t1.kind
+#         of ValueKind.Literal:
+#             false
+#         of ValueKind.Bottom:
+#             true
+#         of ValueKind.Unit:
+#             true
+#         of ValueKind.Bool:
+#             true
+#         of ValueKind.Integer:
+#             t1.bits <= t2.bits
+#         of ValueKind.Float:
+#             true
+#         of ValueKind.Char:
+#             true
+#         of ValueKind.String:
+#             true
+#         of ValueKind.Pair:
+#             `<=`(env, t1.first, t2.first) and `<=`(env, t1.second, t2.second)
+#         of ValueKind.Record:
+#             var res = true
+#             for key in t2.members.keys:
+#                 if key in t1.members:
+#                     if env.`<=`(t1.members[key], t2.members[key]):
+#                         discard
+#                     else:
+#                         res = false
+#                         break
+#                 else:
+#                     res = false
+#                     break
+#             res
+#         of ValueKind.Pi:
+#             # TODO: genty
+#             let
+#                 params = t1.params.zip(t2.params).allIt(env.`<=`(it[1], it[0]))
+#                 rety = env.`<=`(t1.rety, t2.rety)
+#             params and rety
+#         of ValueKind.Singleton:
+#             env.`<=`(t1.base, t2.base)
+#         of ValueKind.Var:
+#             env.`<=`(t1.tv.ub, t2.tv.lb)
+#         of ValueKind.Link:
+#             env.`<=`(t1.to, t2.to)
+#         else:
+#             debug t1.kind
+#             env.raise("notimplemented")
+#             false
+#     # elif t1.kind == ValueKind.Sigma and t2.kind == ValueKind.Sigma:
+#     #     `<=`(env, t1.first, t2.first) and `<=`(env, t1.second, t2.second)
+#     elif t1.kind == ValueKind.Intersection:
+#         t1.types.anyIt(env.`<=`(it, t2))
+#     elif t2.kind == ValueKind.Intersection:
+#         t2.types.allIt(env.`<=`(t1, it))
+#     elif t1.kind == ValueKind.Union:
+#         t1.types.allIt(env.`<=`(it, t2))
+#     elif t2.kind == ValueKind.Union:
+#         t2.types.anyIt(env.`<=`(t1, it))
+#     elif t2.kind == ValueKind.Distinct:
+#         env.`<=`(t1, t2.base)
+#     elif t1.kind == ValueKind.Var:
+#         env.`<=`(t1.tv.ub, t2)
+#     elif t2.kind == ValueKind.Var:
+#         env.`<=`(t1, t2.tv.lb)
+#     elif t1.kind == ValueKind.Unit:
+#         false
+#     elif t2.kind == ValueKind.Bottom:
+#         false
+#     else:
+#         # env.scope.typeOrder.path(t1, t2).isSome
+#         let converters = toSeq(env.scope.converters.keys).filterIt(it[1] == t2)
+#         converters.anyIt(env.`<=`(t1, it[0]))
+# proc `<=?`*(env: TypeEnv, t1, t2: Value): Option[seq[Constraint]] =
+#     proc `<=`(t1, t2: Value): bool =
+#         env.`<=`(t1, t2)
+#     if t1 <= t2:
+#         some newSeq[Constraint]()
+#     elif t1.kind == t2.kind:
+#         case t1.kind
+#         of ValueKind.Bottom, ValueKind.Unit, ValueKind.Integer, ValueKind.Float, ValueKind.Char, ValueKind.String:
+#             some newSeq[Constraint]()
+#         of ValueKind.Pair:
+#             let
+#                 first = env.`<=?`(t1.first, t2.first)
+#                 second = env.`<=?`(t1.second, t2.second)
+#             if first.isSome and second.isSome:
+#                 some first.get & second.get
+#             else:
+#                 none(seq[Constraint])
+#         of ValueKind.Record:
+#             let
+#                 ret = toSeq(t2.members.keys).mapIt(`<=?`(env, t1.members.getOrDefault(it, Value.Unit), t2.members[it]))
+#             if ret.allIt(it.isSome):
+#                 some ret.mapIt(it.get).flatten
+#             else:
+#                 none(seq[Constraint])
+#         of ValueKind.Pi:
+#             let
+#                 params = t1.params.zip(t2.params).mapIt(env.`<=?`(it[1], it[0]))
+#                 rety = env.`<=?`(t1.rety, t2.rety)
+#             if params.allIt(it.isSome) and rety.isSome:
+#                 some params.mapIt(it.get).flatten & rety.get
+#             else:
+#                 none(seq[Constraint])
+#         of ValueKind.Var:
+#             if t1.tv.lb <= t2.tv.ub:
+#                 some @[(t1, t2)]
+#             else:
+#                 none(seq[Constraint])
+#         else:
+#             env.raise("not implemented")
+#             none(seq[Constraint])
+#     elif t2.kind == ValueKind.Var:
+#         # if t2.tv.ub != t1 and t2.tv.ub <= t1:
+#         #     none(seq[Constraint])
+#         # else:
+#         #     some @[(t1, t2)]
+#         if t1 <= t2.tv.ub:
+#             some @[(t1, t2)]
+#         else:
+#             none(seq[Constraint])
+#     elif t1.kind == ValueKind.Var:
+#         # if t2 != t1.tv.lb and t2 <= t1.tv.lb:
+#         #     none(seq[Constraint])
+#         # else:
+#         #     some @[(t1, t2)]
+#         if t1.tv.lb <= t2:
+#             some @[(t1, t2)]
+#         else:
+#             none(seq[Constraint])
+#     else:
+#         if env.scope.typeOrder.path(t1, t2).isSome:
+#             some newSeq[Constraint]()
+#         else:
+#             none(seq[Constraint])
 
 
-proc lub*(self: TypeEnv, t1, t2: Value): Value =
-    setTypeEnv(self)
-    if t1 <= t2:
-        t1
-    elif t2 <= t1:
-        t2
-    else:
-        if t1.kind == t2.kind:
-            self.raise(fmt"{t1} and {t2} can not be unified")
-            Value.Intersection(@[t1, t2])
-        elif t1.kind == ValueKind.Union:
-            Value.Union(t1.types.filter(it => it <= t2))
-        elif t2.kind == ValueKind.Union:
-            self.lub(t2, t1)
-        else:
-            self.raise(fmt"{t1} and {t2} can not be unified")
-            Value.Intersection(@[t1, t2])
-proc glb*(self: TypeEnv, t1, t2: Value): Value =
-    setTypeEnv(self)
-    if t1 <= t2:
-        t2
-    elif t2 <= t1:
-        t1
-    else:
-        if t1.kind == t2.kind:
-            self.raise(fmt"{t1} and {t2} can not be unified")
-            Value.Union(@[t1, t2])
-        elif t1.kind == ValueKind.Intersection:
-            Value.Intersection(t1.types.filter(it => t2 <= it))
-        elif t2.kind == ValueKind.Intersection:
-            self.glb(t2, t1)
-        else:
-            self.raise(fmt"{t1} and {t2} can not be unified")
-            Value.Union(@[t1, t2])
+# proc lub*(self: TypeEnv, t1, t2: Value): Value =
+#     setTypeEnv(self)
+#     if t1 <= t2:
+#         t1
+#     elif t2 <= t1:
+#         t2
+#     else:
+#         if t1.kind == t2.kind:
+#             self.raise(fmt"{t1} and {t2} can not be unified")
+#             Value.Intersection(@[t1, t2])
+#         elif t1.kind == ValueKind.Union:
+#             Value.Union(t1.types.filter(it => it <= t2))
+#         elif t2.kind == ValueKind.Union:
+#             self.lub(t2, t1)
+#         else:
+#             self.raise(fmt"{t1} and {t2} can not be unified")
+#             Value.Intersection(@[t1, t2])
+# proc glb*(self: TypeEnv, t1, t2: Value): Value =
+#     setTypeEnv(self)
+#     if t1 <= t2:
+#         t2
+#     elif t2 <= t1:
+#         t1
+#     else:
+#         if t1.kind == t2.kind:
+#             self.raise(fmt"{t1} and {t2} can not be unified")
+#             Value.Union(@[t1, t2])
+#         elif t1.kind == ValueKind.Intersection:
+#             Value.Intersection(t1.types.filter(it => t2 <= it))
+#         elif t2.kind == ValueKind.Intersection:
+#             self.glb(t2, t1)
+#         else:
+#             self.raise(fmt"{t1} and {t2} can not be unified")
+#             Value.Union(@[t1, t2])
 
