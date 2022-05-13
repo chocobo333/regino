1fad1db730e5b0290e712fd5a2234f3669a0f35d
Unit112358 <yunitto55@gmail.com>
2022-05-14 04:06:10 +0900

    Revise infer for Dot

--- src/sema/eval.nim
+++ src/sema/eval.nim
@@ -100,17 +100,13 @@ proc infer*(self: Expression, env: TypeEnv, global: bool = false): Value =
         env.coerce(Value.Arrow(args.mapIt(Value.Unit), tv) <= callee) # i dont know whether this is correct.
         tv
     of ExpressionKind.Dot:
-        if self.dotArgs.len != 0:
-            let
-                tv = Value.Var(env)
-                args = @[self.lhs.infer(env, global)] & self.dotArgs.mapIt(it.infer(env, global))
-                callee = self.rhs.infer(env, global)
-            env.coerce(callee <= Value.Arrow(args, tv))
-            env.coerce(Value.Arrow(@[Value.Unit], tv) <= callee) # i dont know whether this is correct.
-            tv
-        else:
-            # TODO: not call (ex a.b)
-            Value.Unit
+        let
+            tv = Value.Var(env)
+            args = @[self.lhs.infer(env, global)] & self.dotArgs.mapIt(it.infer(env, global))
+            callee = self.rhs.infer(env, global)
+        env.coerce(callee <= Value.Arrow(args, tv))
+        env.coerce(Value.Arrow(@[Value.Unit], tv) <= callee) # i dont know whether this is correct.
+        tv
     of ExpressionKind.Bracket:
         Value.Unit
     of ExpressionKind.Binary:
