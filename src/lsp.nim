175ba4954f0023eb0cfec6b2fdf645fbf5fe083a
chocobo333 <chocobo1355@gmail.com>
2022-05-14 18:09:32 +0900

    make available

--- src/lsp.nim
+++ src/lsp.nim
@@ -172,8 +172,7 @@ proc `textDocument/hover`(s: Stream, msg: RequestMessage, configuration: Configu
                 if not focus.typ.isNil and focus.typ.symbol.isSome:
                     data.add $focus.typ.symbol.get
                     if focus.typ.symbol.get.kind == il.SymbolKind.Func:
-                        if focus.typ.symbol.get.decl_funcdef.docStr.isSome:
-                            data.add focus.typ.symbol.get.decl_funcdef.docStr.get.join("\n")
+                        data.add focus.typ.symbol.get.decl_funcdef.docStr.join("\n")
                 data.add fmt"{focus.name}: {focus.typ}"
         s.respond(msg):
             if data.len == 0:
