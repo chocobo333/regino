9935ef2bb2127f1435a68abbe81b8238910b8c02
chocobo333 <chocobo1355@gmail.com>
2022-07-08 19:21:41 +0900

    fix

--- src/sema/projects.nim
+++ src/sema/projects.nim
@@ -11,7 +11,7 @@ type
     Buffer*[T] = (Project, Table[string, T])
     Project* = ref object
         main*: string
-        programs*: Buffer[Expression]
+        programs*: Buffer[ir.Expression]
         errs*: seq[Error]
         env*: TypeEnv
 
@@ -20,7 +20,7 @@ proc newBuffer*[T](p: Project): Buffer[T] =
     (p, initTable[string, T]())
 proc newProject*(main: string): Project =
     result = Project(main: main)
-    result.programs = newBuffer[Expression](result)
+    result.programs = newBuffer[ir.Expression](result)
 
 proc addErr*(self: Project, err: Option[Error]) =
     if err.isSome:
