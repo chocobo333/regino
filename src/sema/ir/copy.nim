d40e5dd2fe5f4eb280dc9a11d1929692959d1a48
Unit112358 <yunitto55@gmail.com>
2022-07-09 13:28:10 +0900

    Impl copy for Type

--- src/sema/ir/copy.nim
+++ src/sema/ir/copy.nim
@@ -6,20 +6,31 @@ import ir
 import constructors
 
 proc copy*(self: Pattern): Pattern
+proc copy*(self: GenericType): GenericType
+proc copy*(self: FunctionSignature): FunctionSignature
+proc copy*(self: Function): Function
+
+proc copy*(self: PiType): PiType =
+    # TODO:
+    self
 proc copy*[A, B](self: (A, B)): (A, B) =
     let (a, b) = self
     (a.copy, b.copy)
 proc copy*(self: Pattern): Pattern =
     # TODO:
-    case self.kind
-    of PatternKind.Literal:
-        Pattern.Literal
-    of PatternKind.Ident:
-        Pattern.Ident
-    of PatternKind.Tuple:
-        Pattern.Tuple
-    of PatternKind.Record:
-        Pattern.Record
+    self
+    # case self.kind
+    # of PatternKind.Literal:
+    #     Pattern.Literal
+    # of PatternKind.Ident:
+    #     Pattern.Ident
+    # of PatternKind.Tuple:
+    #     Pattern.Tuple
+    # of PatternKind.Record:
+    #     Pattern.Record
+proc copy*(self: Value): Value = 
+    # TODO:
+    self
 proc copy*(self: Type): Type =
     case self.kind
     of TypeKind.Bottom:
@@ -29,7 +40,9 @@ proc copy*(self: Type): Type =
     of TypeKind.Univ:
         Type.Univ(self.level)
     of TypeKind.Value:
-        Type.Value(self.val)
+        Type.value(self.val.copy)
+    of TypeKind.Bool:
+        Type.Bool
     of TypeKind.Integer:
         Type.Integer(self.nbits)
     of TypeKind.Float:
@@ -50,10 +63,18 @@ proc copy*(self: Type): Type =
         for k in self.members.keys:
             m[k] = self.members[k].copy
         Type.Object(m)
+    of TypeKind.Array:
+        Type.Array(self.length, self.base.copy)
+    of TypeKind.Distinct:
+        Type.Distinct(self.base.copy)
+    of TypeKind.Singleton:
+        Type.Singleton(self.base.copy)
+    of TypeKind.Ptr:
+        Type.Ptr(self.pointee.copy)
     of TypeKind.Arrow:
-        Type.Arrow(self.params.map(copy), self.rety)
+        Type.Arrow(self.params.map(copy), self.rety.copy)
     of TypeKind.Cons:
-        Type.Cons(self.cons, self.args.map(copy))
+        Type.Cons(self.cons.copy, self.args.map(copy))
     of TypeKind.Recursive:
         Type.Recursive(self.self, self.body.copy)
     of TypeKind.Trait:
@@ -61,6 +82,17 @@ proc copy*(self: Type): Type =
     of TypeKind.Var:
         Type.Link(self)
     of TypeKind.Gen:
-        Type.Gen(self.gt)
+        Type.Gen(self.gt.copy)
     of TypeKind.Link:
         Type.Link(self.to.copy)
+
+proc copy*(self: GenericType): GenericType =
+    # TODO:
+    self
+
+proc copy*(self: FunctionSignature): FunctionSignature =
+    # TODO:
+    self
+proc copy*(self: Function): Function =
+    # TODO:
+    self
