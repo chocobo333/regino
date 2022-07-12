aae6de739dcab2c75d6b3d20247215a35b388389
Unit112358 <yunitto55@gmail.com>
2022-07-12 13:49:56 +0900

    Impl il2ir for il.Project

--- src/il2ir.nim
+++ src/il2ir.nim
@@ -1,11 +1,15 @@
 
 import sequtils
+import tables
 
 import syntax/il
+import syntax/projects as ilprojects
+import sema/projects as irprojects
 import sema/ir/[
-    ir,
+    ir, 
     constructors
 ]
+
 import lineinfos
 
 proc il2ir*(self: Statement): ir.Expression =
@@ -21,3 +25,8 @@ proc il2ir*(self: Program): ir.Expression =
         # change definition of Program to have location and revise parser of Program
         newLocation()
     )
+
+proc il2ir*(self: ilprojects.Project): irprojects.Project =
+    result = irprojects.newProject(self.main)
+    for uri, program in self.program.pairs:
+        result.programs[uri] = program.il2ir
