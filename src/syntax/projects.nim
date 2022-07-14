32fc3e4346d97640a5127d6c315087ea52a1a4f4
Unit112358 <yunitto55@gmail.com>
2022-07-14 17:19:52 +0900

    Impl buildProject

--- src/syntax/projects.nim
+++ src/syntax/projects.nim
@@ -1,16 +1,28 @@
 
 import tables
+import options
 
 import il
+import parsers
 
 type
     Buffer*[T] = Table[string, T]
     Project* = ref object
         main*: string
-        program*: Buffer[Program]
+        program*: Buffer[il.Program]
 
 proc newBuffer*[T](): Buffer[T] =
     initTable[string, T]()
 proc newProject*(main: string): Project =
     result = Project(main: main)
-    result.program = newBuffer[Program]()
+    result.program = newBuffer[il.Program]()
+
+proc buildProject*(main: string): Project =
+    let 
+        f = open(main)
+        s = f.readAll
+        program = Program(Source.from(s)).get
+        project = newProject(main)
+    
+    project.program[main] = program
+    project
